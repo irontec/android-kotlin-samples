50fb625015cd8c7a6739f5ab8ebb943f7e2a80c0
Axier Fernandez <afernandez@irontec.com>
2017-06-14 17:49:27 +0200

    Updated Anko_asynchronous_tasks to the latest Kotlin and Anko versions and fixed some minor bugs

--- Anko_asynchronous_tasks/app/src/main/java/com/irontec/kotlintest/MainActivity.kt
+++ Anko_asynchronous_tasks/app/src/main/java/com/irontec/kotlintest/MainActivity.kt
@@ -1,72 +1,68 @@
 package com.irontec.kotlintest
 
-import android.app.Activity
-import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.TextView
-import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.irontec.kotlintest.deserializers.WeatherDeserializer
 import com.irontec.kotlintest.models.WeatherObject
 import com.irontec.kotlintest.networking.NetworkClient
-import org.json.JSONObject
+import org.jetbrains.anko.*
 import java.io.BufferedInputStream
 import java.io.BufferedReader
 import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.URL
-import org.jetbrains.anko.*
 
 
-public class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super<AppCompatActivity>.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState)
 
         verticalLayout {
             padding = dip(5)
             val mTextView = textView {
-                textSize = 12f
-            }.layoutParams(width = matchParent, height = dip(300))
+                textSize = 16f
+                width = matchParent
+                height = dip(300)
+            }
             button("Retrieve Weather") {
                 textSize = 14f
+                width = matchParent
+                height = wrapContent
                 onClick {
                     // Declare a background asynchronous task, no need for an AsyncTask anymore
-                    async {
+                    doAsync {
                         // Heavy duty work made on the background
                         val networkClient = NetworkClient()
                         val stream = BufferedInputStream(
                                 networkClient.get("https://raw.githubusercontent.com/irontec/android-kotlin-samples/master/common-data/bilbao.json"))
                         val bufferedReader = BufferedReader(InputStreamReader(stream));
-                        val stringBuilder = StringBuilder();
+                        val stringBuilder = StringBuilder()
                         bufferedReader.forEachLine { stringBuilder.append(it) }
-                        val gsonBuilder = GsonBuilder().serializeNulls();
-                        gsonBuilder.registerTypeAdapter(javaClass<WeatherObject>(), WeatherDeserializer());
-                        val gson = gsonBuilder.create();
-                        val weather = gson.fromJson(stringBuilder.toString(), javaClass<WeatherObject>()) as WeatherObject
+                        val gsonBuilder = GsonBuilder().serializeNulls()
+                        gsonBuilder.registerTypeAdapter(WeatherObject::class.java, WeatherDeserializer());
+                        val gson = gsonBuilder.create()
+                        val weather = gson.fromJson(stringBuilder.toString(), WeatherObject::class.java)
 
                         uiThread {
                             // Just set the TextView text on the UI-Thread
-                            mTextView.setText(weather.toString())
+                            mTextView.text = weather.toString()
                         }
                     }
                 }
-            }.layoutParams(width = matchParent, height = wrapContent)
+            }
         }
 
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        getMenuInflater().inflate(R.menu.menu_main, menu)
+        menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.getItemId()
+        val id = item.itemId
         if (id == R.id.action_settings) {
             return true
         }
--- Anko_asynchronous_tasks/app/src/main/java/com/irontec/kotlintest/models/WeatherObject.kt
+++ Anko_asynchronous_tasks/app/src/main/java/com/irontec/kotlintest/models/WeatherObject.kt
@@ -3,22 +3,27 @@ package com.irontec.kotlintest.models
 /**
  * Created by axier on 24/4/15.
  */
-public class WeatherObject {
+class WeatherObject {
 
-    public var main: String = ""
-    public var description: String = ""
-    public var temp: Float = 0.0f
-    public var temp_Max: Float = 0.0f
-    public var temp_Min: Float = 0.0f
-    public var humidity: Int = 0
-    public var wind: WindObject? = null
+    var main: String = ""
+    var description: String = ""
+    var temp: Float = 0.0f
+    var temp_Max: Float = 0.0f
+    var temp_Min: Float = 0.0f
+    var humidity: Int = 0
+    var wind: WindObject? = null
 
     init {
         println(this.toString())
     }
 
     override fun toString() =
-            "Main ${main}, description ${description}, temp ${temp}, temp_Max ${temp_Max}," +
-                ", temp_Min ${temp_Min}, humidity ${humidity}, wind ${wind.toString()}"
+            "Main ${main}\n" +
+            "description ${description}\n" +
+            "temp ${temp}\n" +
+            "temp_Max ${temp_Max}\n" +
+            "temp_Min ${temp_Min}\n" +
+            "humidity ${humidity}\n" +
+            "wind ${wind.toString()}"
 
 }
