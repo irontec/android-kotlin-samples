edf9084bfc531ebaf9c5dbaf1787e379b1679b4e
Axier Fernandez <afernandez@irontec.com>
2017-06-15 08:59:28 +0200

    Updated HttpUrlConnection_AsyncTask to the latest version of Kotlin using synthetic properties

--- HttpUrlConnection_AsyncTask/app/src/main/java/com/irontec/kotlintest/MainActivity.kt
+++ HttpUrlConnection_AsyncTask/app/src/main/java/com/irontec/kotlintest/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.irontec.kotlintest
 
 import android.os.AsyncTask
-import android.support.v7.app.ActionBarActivity
 import android.os.Bundle
-import android.util.Log
+import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
 import java.io.BufferedInputStream
 import java.io.BufferedReader
@@ -15,19 +15,13 @@ import java.net.HttpURLConnection
 import java.net.URL
 
 
-public class MainActivity : ActionBarActivity() {
-
-    open val mTextView: TextView
-        get() {
-            return findViewById(R.id.text) as TextView
-        }
-
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        GetWeatherTask(mTextView).execute()
+        GetWeatherTask(this.text).execute()
     }
 
     class GetWeatherTask(textView: TextView) : AsyncTask<Void, Void, String>() {
@@ -37,25 +31,25 @@ public class MainActivity : ActionBarActivity() {
         override fun doInBackground(vararg params: Void?): String? {
             val url = URL("https://raw.githubusercontent.com/irontec/android-kotlin-samples/master/common-data/bilbao.json")
             val httpClient = url.openConnection() as HttpURLConnection
-            if(httpClient.getResponseCode() == HttpURLConnection.HTTP_OK){
+            if (httpClient.responseCode == HttpURLConnection.HTTP_OK) {
                 try {
-                    val stream = BufferedInputStream(httpClient.getInputStream())
+                    val stream = BufferedInputStream(httpClient.inputStream)
                     val data: String = readStream(inputStream = stream)
-                    return data;
-                } catch (e : Exception) {
+                    return data
+                } catch (e: Exception) {
                     e.printStackTrace()
                 } finally {
                     httpClient.disconnect()
                 }
-            }else{
-                println("ERROR ${httpClient.getResponseCode()}")
+            } else {
+                println("ERROR ${httpClient.responseCode}")
             }
             return null
         }
 
         fun readStream(inputStream: BufferedInputStream): String {
-            val bufferedReader = BufferedReader(InputStreamReader(inputStream));
-            val stringBuilder = StringBuilder();
+            val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+            val stringBuilder = StringBuilder()
             bufferedReader.forEachLine { stringBuilder.append(it) }
             return stringBuilder.toString()
         }
@@ -67,7 +61,7 @@ public class MainActivity : ActionBarActivity() {
         override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
 
-            innerTextView?.setText(JSONObject(result).toString())
+            innerTextView?.text = JSONObject(result).toString()
 
             /**
              * ... Work with the weather data
@@ -77,12 +71,12 @@ public class MainActivity : ActionBarActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        getMenuInflater().inflate(R.menu.menu_main, menu)
+        menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.getItemId()
+        val id = item.itemId
         if (id == R.id.action_settings) {
             return true
         }
