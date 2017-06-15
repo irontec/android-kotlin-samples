c9ae19c0d87f2fa42570eb9d67e551d709504c3d
Axier Fernandez <afernandez@irontec.com>
2017-06-15 09:15:23 +0200

    Updated OkHttp_AsyncTask to the latest Kotlin version. Added synthetic properties and fixed minor bugs

--- OkHttp_AsyncTask/app/src/main/java/com/irontec/kotlintest/MainActivity.kt
+++ OkHttp_AsyncTask/app/src/main/java/com/irontec/kotlintest/MainActivity.kt
@@ -1,70 +1,60 @@
 package com.irontec.kotlintest
 
 import android.os.AsyncTask
-import android.support.v7.app.ActionBarActivity
 import android.os.Bundle
-import android.util.Log
+import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.TextView
 import com.irontec.kotlintest.networking.NetworkClient
-import org.json.JSONObject
+import kotlinx.android.synthetic.main.activity_main.*
 import java.io.BufferedInputStream
 import java.io.BufferedReader
 import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.URL
 
-
-public class MainActivity : ActionBarActivity() {
-
-    open val mTextView: TextView
-        get() {
-            return findViewById(R.id.text) as TextView
-        }
-
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        GetJsonWithOkHttpClient(mTextView).execute()
+        GetJsonWithOkHttpClient(this.text).execute()
 
     }
 
-    open class GetJsonWithOkHttpClient(textView: TextView) : AsyncTask<Void, Void, String>() {
+    open class GetJsonWithOkHttpClient(textView: TextView) : AsyncTask<Unit, Unit, String>() {
 
         val mInnerTextView = textView
 
-        override fun doInBackground(vararg params: Void?): String? {
+        override fun doInBackground(vararg params: Unit?): String? {
             val networkClient = NetworkClient()
             val stream = BufferedInputStream(
                     networkClient.get("https://raw.githubusercontent.com/irontec/android-kotlin-samples/master/common-data/bilbao.json"))
-            return readStream(stream);
+            return readStream(stream)
         }
 
         override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
 
-            mInnerTextView.setText(result)
+            mInnerTextView.text = result
 
         }
 
         fun readStream(inputStream: BufferedInputStream): String {
-            val bufferedReader = BufferedReader(InputStreamReader(inputStream));
-            val stringBuilder = StringBuilder();
+            val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+            val stringBuilder = StringBuilder()
             bufferedReader.forEachLine { stringBuilder.append(it) }
             return stringBuilder.toString()
         }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        getMenuInflater().inflate(R.menu.menu_main, menu)
+        menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.getItemId()
+        val id = item.itemId
         if (id == R.id.action_settings) {
             return true
         }
