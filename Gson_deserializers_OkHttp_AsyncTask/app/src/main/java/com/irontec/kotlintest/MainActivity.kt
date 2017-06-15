70d409b9bf0f26089e28cc02061da9340e9bc97b
Axier Fernandez <afernandez@irontec.com>
2017-06-15 08:49:16 +0200

    Updated Gson_deserializers_OkHttp_AsyncTask to the latest version of Kotlin with synthetic properties

--- Gson_deserializers_OkHttp_AsyncTask/app/src/main/java/com/irontec/kotlintest/MainActivity.kt
+++ Gson_deserializers_OkHttp_AsyncTask/app/src/main/java/com/irontec/kotlintest/MainActivity.kt
@@ -3,78 +3,69 @@ package com.irontec.kotlintest
 import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.TextView
-import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.irontec.kotlintest.deserializers.WeatherDeserializer
 import com.irontec.kotlintest.models.WeatherObject
 import com.irontec.kotlintest.networking.NetworkClient
-import org.json.JSONObject
+import kotlinx.android.synthetic.main.activity_main.*
 import java.io.BufferedInputStream
 import java.io.BufferedReader
 import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.URL
 
 
-public class MainActivity : AppCompatActivity() {
-
-    open val mTextView: TextView
-        get() {
-            return findViewById(R.id.text) as TextView
-        }
-
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        GetJsonWithOkHttpClient(mTextView).execute()
+        //synthetic properties
+        GetJsonWithOkHttpClient(this.text).execute()
 
     }
 
-    open class GetJsonWithOkHttpClient(textView: TextView) : AsyncTask<Void, Void, String>() {
+    open class GetJsonWithOkHttpClient(textView: TextView) : AsyncTask<Unit, Unit, String>() {
 
         val mInnerTextView = textView
 
-        override fun doInBackground(vararg params: Void?): String? {
+        override fun doInBackground(vararg params: Unit?): String? {
             val networkClient = NetworkClient()
             val stream = BufferedInputStream(
                     networkClient.get("https://raw.githubusercontent.com/irontec/android-kotlin-samples/master/common-data/bilbao.json"))
-            return readStream(stream);
+            return readStream(stream)
         }
 
         override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
 
 
-            val gsonBuilder = GsonBuilder().serializeNulls();
-            gsonBuilder.registerTypeAdapter(javaClass<WeatherObject>(), WeatherDeserializer());
-            val gson = gsonBuilder.create();
-            val weather = gson.fromJson(result, javaClass<WeatherObject>()) as WeatherObject
+            val gsonBuilder = GsonBuilder().serializeNulls()
+            gsonBuilder.registerTypeAdapter(WeatherObject::class.java, WeatherDeserializer())
+            val gson = gsonBuilder.create()
+            val weather = gson.fromJson(result, WeatherObject::class.java)
 
-            mInnerTextView.setText(weather.toString())
+            mInnerTextView.text = weather.toString()
 
         }
 
         fun readStream(inputStream: BufferedInputStream): String {
-            val bufferedReader = BufferedReader(InputStreamReader(inputStream));
-            val stringBuilder = StringBuilder();
+            val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+            val stringBuilder = StringBuilder()
             bufferedReader.forEachLine { stringBuilder.append(it) }
             return stringBuilder.toString()
         }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        getMenuInflater().inflate(R.menu.menu_main, menu)
+        menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.getItemId()
+        val id = item.itemId
         if (id == R.id.action_settings) {
             return true
         }
